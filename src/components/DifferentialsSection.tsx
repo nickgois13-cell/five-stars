be739b509c091e7949ae3f0250acc42e5407a1a4
nickgois13-cell <nick.gois13@gmail.com>
2026-04-17 19:32:32 -0300

    Update DifferentialsSection.tsx

--- src/components/DifferentialsSection.tsx
+++ src/components/DifferentialsSection.tsx
@@ -28,7 +28,7 @@ const DifferentialsSection = () => {
   const ref = useScrollReveal();
 
   return (
-    <section className="py-24 bg-chocolate-gradient relative overflow-hidden">
+    <section className="py-24 bg-chocolate relative overflow-hidden">
       {/* Decorative elements */}
       <div className="absolute top-0 left-0 w-64 h-64 bg-gold/5 rounded-full blur-3xl -translate-x-1/2 -translate-y-1/2" />
       <div className="absolute bottom-0 right-0 w-96 h-96 bg-gold/5 rounded-full blur-3xl translate-x-1/2 translate-y-1/2" />
